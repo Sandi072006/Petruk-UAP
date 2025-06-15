e92b1d62b9cc256669e8cd7a8039db9ac4d0ccf3
davaismifadil <davaismif@gmail.com>
2025-06-15 18:31:03 +0700

    Update Petruk_Kel5.cpp

--- Petruk_Kel5.cpp
+++ Petruk_Kel5.cpp
@@ -16,6 +16,7 @@ private:
     char komentar[100];
 
  double komponenNilai[6];
+ char komentar[100];
     
     queue<string> urutanInput;
     double persenUAS, persenUTS, persenQuiz, persenTugas, persenAbsensi, persenResponsi;
@@ -47,6 +48,56 @@ public:
         persenUAS = 0, persenUTS = 0, persenQuiz = 0, persenTugas = 0, persenAbsensi = 0, persenResponsi = 0, 
         nilaiUAS = 0, nilaiUTS = 0, nilaiQuiz = 0, nilaiTugas = 0, nilaiAbsensi = 0, nilaiResponsi = 0;
     }
+void setNama(string n) { 
+        nama = n;
+    }
+    void setSKS(int s) { 
+        sks = s; 
+    }
+    string getNama() { 
+        return nama; 
+    }
+    int getSKS() { 
+        return sks; 
+    } 
+    double getNilaiAkhir() { 
+        return nilaiAkhir; 
+    }
+    string getHurufMutu() { 
+        return hurufMutu; 
+    }
+    double getBobotMutu() { 
+        return bobotMutu; 
+    }
+
+void konversiNilai(double nilai) {
+        if (nilai >= 85) {
+            hurufMutu = "A+";
+            bobotMutu = 4.0;
+        } else if (nilai >= 80) {
+            hurufMutu = "A";
+            bobotMutu = 4.0;
+        } else if (nilai >= 75) {
+            hurufMutu = "B+";
+            bobotMutu = 3.5;
+        } else if (nilai >= 70) {
+            hurufMutu = "B";
+            bobotMutu = 3.0;
+        } else if (nilai >= 65) {
+            hurufMutu = "C+";
+            bobotMutu = 2.5;
+        } else if (nilai >= 60) {
+            hurufMutu = "C";
+            bobotMutu = 2.0;
+        } else if (nilai >= 50) {
+            hurufMutu = "D";
+            bobotMutu = 1.0;
+        } else {
+            hurufMutu = "E";
+            bobotMutu = 0.0;
+        }
+    }
+    
 }
 
 
